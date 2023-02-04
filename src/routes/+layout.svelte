a28bd64da863ead01a77d4820012f3b8e48ff90e
Aqirito <aqil.rahmat99@gmail.com>
2023-02-04 19:56:35 +0800

    UPDATE: create base layout page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import Header from "../lib/components/Header.svelte";
+  import PublicHeader from "../lib/components/PublicHeader.svelte";
+</script>
+
+<Header></Header>
+<PublicHeader></PublicHeader>
+<main>
+  <slot />
+</main>
