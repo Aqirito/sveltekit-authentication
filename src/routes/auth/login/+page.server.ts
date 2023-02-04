88282c7af624006b554dd88862152b4cc2d0812a
Aqirito <aqil.rahmat99@gmail.com>
2023-02-04 22:37:36 +0800

    UPDATE: change form from username to phone_no

--- src/routes/auth/login/+page.server.ts
+++ src/routes/auth/login/+page.server.ts
@@ -1,41 +1,47 @@
 import { fail, redirect } from '@sveltejs/kit';
 import type { Actions } from './$types';
+const apiUrl = import.meta.env.VITE_API_URL;
  
 export const actions = {
-  default: async ({ request }) => {
+  default: async ({ request, locals, fetch }) => {
     // TODO log the user in
     // console.log('login request', request);
     const formData = await request.formData();
     console.log('login formData', formData)
 
     // seperate the data for validations
-    const username = formData.get('username');
+    const phone_no = formData.get('phone_no');
     const password = formData.get('password');
 
     // TODO you can create validations here
-    if (!username) {
+    if (!phone_no) {
       return fail(400, {
         success: false,
-        message: "Username is Required!.",
+        message: "Phone number is Required!.",
         // the data is dissapear after form actions is accur
         // return the from value to get back the value submitted
         // so user will not rewrite the forms from zero 
         value: {
-          username,
+          phone_no,
           password
         }
       })
     }
-    if (username.length < 2) {
+    if (phone_no.length < 9) {
       return fail(400, {
         success: false,
-        message: "username must be atleast two characters"
+        message: "Phone number must be atleast two characters"
       })
     }
-
-    throw redirect(303, '/');
-    // return {
-    //   success: true
-    // }
+    if (!password) {
+      return fail(400, {
+        success: false,
+        message: "Password is required!"
+      })
+    }
+    // throw redirect(303, '/');
+    return {
+      success: true
+    }
   }
 } satisfies Actions;
