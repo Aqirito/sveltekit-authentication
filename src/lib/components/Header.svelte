ba45a666f56f89ae0fa3b77a258fbbecd0ec88fd
Aqirito <aqil.rahmat99@gmail.com>
2023-02-04 19:55:22 +0800

    UPDATE: create header and public header component

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,29 @@
+<header>
+  <ul class="top-nav">
+    <li><a href="/">Home</a></li>
+    <li><a href="public">Public</a></li>
+    <li><a href="admin">Admin</a></li>
+    <li><a href="#">Logout</a></li>
+  </ul>
+</header>
+
+<style>
+  header {
+    display: flex;
+    justify-content: center;
+    background-color: rgb(166, 255, 227);
+  }
+  ul.top-nav {
+    padding: 10px;
+  }
+  ul.top-nav li {
+    cursor: pointer;
+    padding: 10px;
+    display: inline;
+    border: solid rgb(219, 101, 101);
+    border-radius: 10px;
+  }
+  ul.top-nav li:hover {
+    background-color: rgb(118, 244, 204);
+  }
+</style>
---
+++ src/lib/components/PublicHeader.svelte
@@ -0,0 +1,28 @@
+<header>
+  <ul class="top-nav">
+    <li><a href="/">Home</a></li>
+    <li><a href="/auth/login">Login</a></li>
+    <li><a href="/auth/register">Register</a></li>
+  </ul>
+</header>
+
+<style>
+  header {
+    display: flex;
+    justify-content: center;
+    background-color: rgb(166, 255, 227);
+  }
+  ul.top-nav {
+    padding: 10px;
+  }
+  ul.top-nav li {
+    cursor: pointer;
+    padding: 10px;
+    display: inline;
+    border: solid rgb(219, 101, 101);
+    border-radius: 10px;
+  }
+  ul.top-nav li:hover {
+    background-color: rgb(118, 244, 204);
+  }
+</style>
