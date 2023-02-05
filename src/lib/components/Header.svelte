2dbb5b7ece444159133c9ecb736e76b47ca55329
Aqirito <aqil.rahmat99@gmail.com>
2023-02-06 04:10:55 +0800

    UPDATE: headers component

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,8 +1,8 @@
 <header>
   <ul class="top-nav">
     <li><a href="/">Home</a></li>
-    <li><a href="public">Public</a></li>
-    <li><a href="admin">Admin</a></li>
+    <li><a href="/public">Public</a></li>
+    <li><a href="/admin">Admin</a></li>
     <li><a href="#">Logout</a></li>
   </ul>
 </header>
